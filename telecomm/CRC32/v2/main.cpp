14c6adc0b2c236b061dbb775def5b9a7fcfa2363
Meow-pilli <149984898+Meow-pilli@users.noreply.github.com>
2024-07-19 02:57:49 -0500

    main.cpp

--- telecomm/CRC32/v2/main.cpp
+++ telecomm/CRC32/v2/main.cpp
@@ -1,50 +1,61 @@
-//========================================================================================
-// 
+//===========================================================================
+//
 //
 // File Name    : main.cpp
-// Description  : Testbench for quick sort
-// Release Date : 01/08/2013
+// Description  : crc32 CIPHER and INV CHIPER  main
+// Release Date : 24/11/2014 
 // Author       : PolyU DARC Lab
-//                Benjamin Carrion Schafer, Anushree Mahapatra
+//                Benjamin Carrion Schafer, Shuangnan Liu
 //
 // Revision History
-//---------------------------------------------------------------------------------------
-// Date        Version      Author                 Description
-//----------------------------------------------------------------------------------------
-//01/08/2013    1.0         PolyU  DARC Lab      Quick sort main module
+//---------------------------------------------------------------------------
+// Date             Author      Version     Description
+//---------------------------------------------------------------------------
+//14/02/2013        PolyU           1.0     Main system description
+//24/11/2014        PolyU           1.1     cipher and inv cipher combined into same file
 //
-// g++ -o qsort.exe main.cpp tb_qsort.cpp qsort.cpp -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib -lsystemc -lm
-//=======================================================================================
+// g++ -o crc32_cipher.exe main.cpp tb_crc32_cipher_3G.cpp crc32_cipher.cpp -I$SYSTEMC_HOME/include -L$SYSTEMC_HOME/lib -lsystemc -lm
+//===========================================================================
 
-
-#include "qsort.h"
-#include "tb_qsort.h"
+#include "crc32.h"
+#include "tb_crc32.h"
 
 
 int sc_main(int argc, char** argv)
 {
   sc_clock                clk("clk", 25, SC_NS, 0.5, 12.5, SC_NS, true);
   sc_signal<bool>         rst;
- 
-   sc_signal<sc_uint<8> > indata;
-   sc_signal<sc_uint<8> > odata;
+  sc_signal<sc_uint<8> >  in_data[SIZE];
+  sc_signal<sc_uint<8> >  out_data[SIZE];
 
-   quicksort u_QSORT("QSORT");
-   test_qsort u_test("test_QSORT");
 
-   //connect to bubble sort
-   u_QSORT.clk( clk );
-   u_QSORT.rst( rst );
+  int x;
+  char name[10];
 
-   u_QSORT.indata( indata);
-   u_QSORT.odata(  odata );
- 
-  // connect to test bench
-  u_test.clk( clk );
-  u_test.rst( rst );
-  
-  u_test.indata( indata );
-  u_test.odata( odata );
+
+  crc32 	 u_crc32("crc32");
+  test_crc32 test("test_crc32");
+
+  //connect to crc32
+  u_crc32.clk( clk );
+  u_crc32.rst( rst );
+ // u_crc32.mode( mode );
+
+  for(x=0; x < SIZE; x++){
+    u_crc32.idata[x]( in_data[x] );
+    u_crc32.odata[x]( out_data[x] );
+  }
+
+
+  // connect to Test Bench
+  test.clk( clk );
+  test.rst( rst );
+ // test.mode( mode );
+
+  for(x=0; x < SIZE; x++){
+    test.idata[x]( in_data[x] );
+    test.odata[x]( out_data[x] );
+  }
 
 
 #ifdef WAVE_DUMP
@@ -52,22 +63,31 @@ int sc_main(int argc, char** argv)
   sc_trace_file* trace_file = sc_create_vcd_trace_file("trace_behav");
 
   // Top level signals
-  sc_trace(trace_file, clk              , "clk");
-  sc_trace(trace_file, rst              , "rst");
+  sc_trace(trace_file, clk             , "clk");
+  sc_trace(trace_file, rst             , "rst");
+
+ for(x=0; x < SIZE; x++){
+   sprintf(name, "in_data%d",x);   
+   sc_trace(trace_file, in_data[x]          , name);
+
+   sprintf(name, "out_data%d",x);   
+   sc_trace(trace_file, out_data[x]            , name);
  
-   sc_trace(trace_file, indata          , "indata"); 
-   sc_trace(trace_file,  odata            ,"odata");
- 
+  }
+
 #endif  // End WAVE_DUMP
 
   sc_start( 25, SC_NS );
   rst.write(0);
+  //mode.write(0);  // Encrypt DATA. Set to 1 to decrypt
 
+  
   sc_start( 25, SC_NS );
   rst.write(1);
 
+  
+  sc_start(1000, SC_NS); 
   sc_start();
-
 #ifdef WAVE_DUMP
    sc_close_vcd_trace_file(trace_file);
 #endif
@@ -75,3 +95,8 @@ int sc_main(int argc, char** argv)
     return 0;
 
 };
+
+
+
+
+
